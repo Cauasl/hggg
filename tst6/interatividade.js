555e6a4c24ac6995b808442aa7e6a8b6a614fae2
Cauasl <cacasaraivalopes@gmail.com>
2023-12-24 12:40:55 -0300

    Atualizacao

--- tst6/interatividade.js
+++ tst6/interatividade.js
@@ -1,12 +1,24 @@
-function lala() {
+(function() {
 	let sec = document.querySelector("section#personagens");
 	let t = (sec.querySelectorAll('img').length) - 1;
+	let tela = document.querySelector("#tela");
+	let texto = ['Mario', 'Luigi', 'Peach', 'Bowser', 'DK', 'Toad', 'Wario', 'Yoshi'];
 	
+	//X
+	tela.getElementsByTagName('p')[0].addEventListener("click", function() {
+		tela.getElementsByTagName('img')[0].setAttribute("src", "");
+		tela.setAttribute('style', 'display: none;');
+		
+	});
+	
+	//Imagens
 	for(var i=0; i <= t; i++) {
 		let k = sec.getElementsByTagName('img')[i].src;
+		let n = i;
 		sec.getElementsByTagName('img')[i].addEventListener("click", function() {
-			alert(`aqui ${k}`);
+			tela.getElementsByTagName('img')[0].setAttribute("src", `${k}`);
+			tela.setAttribute('style', 'display: block;');
+			tela.getElementsByTagName('p')[1].innerHTML = texto[n];
 		});
-		
 	}
-}
+}());
