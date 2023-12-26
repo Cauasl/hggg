a5a79f88eb56a82b5e9d6dd7be6478ba237c637e
Cauã S. Lopes <154275368+Cauasl@users.noreply.github.com>
2023-12-26 11:52:44 -0300

    a

--- tst6/interatividade.js
+++ tst6/interatividade.js
@@ -3,7 +3,14 @@
 	let sec = document.querySelector("section#personagens");
 	let t = (sec.querySelectorAll('img').length) - 1;
 	let tela = document.querySelector("#tela");
-	let texto = ['Mario', 'Luigi', 'Peach', 'Bowser', 'DK', 'Toad', 'Wario', 'Yoshi'];
+	let texto = ['Um circuito simples com curvas suaves e algumas rampas. Possui a temática clássica do Reino do Cogumelo.',
+	'Localizado em uma fazenda, o percurso tem curvas fechadas e obstáculos como Monty Moles e vacas.',
+	'Uma montanha de chocolate com terreno acidentado e uma avalanche ocasional de rochas de chocolate.',
+	'Um castelo vulcânico com obstáculos perigosos, como Thwomps, lava e pontes móveis.',
+	'Uma pista ambientada na selva com curvas fechadas, uma ponte estreita e uma seção subaquática.',
+	'Situado em uma autoestrada movimentada, os jogadores devem evitar carros, caminhões e ônibus enquanto correm.',
+	'Uma pista longa com muitos saltos e obstáculos. Exige habilidade para evitar quedas e atalhos estratégicos.',
+	'Um circuito confuso com múltiplos caminhos possíveis, dificultando a determinação de quem está na liderança.'];
 	
 	//X
 	tela.getElementsByTagName('p')[0].addEventListener("click", function() {
@@ -15,10 +22,12 @@
 	//Imagens
 	for(var i=0; i <= t; i++) {
 		let k = sec.getElementsByTagName('img')[i].src;
+		let name = sec.getElementsByTagName('img')[i].alt;
 		let n = i;
 		sec.getElementsByTagName('img')[i].addEventListener("click", function() {
 			tela.getElementsByTagName('img')[0].setAttribute("src", `${k}`);
 			tela.setAttribute('style', 'display: block;');
+			tela.getElementsByTagName('h2')[0].innerHTML = name;
 			tela.getElementsByTagName('p')[1].innerHTML = texto[n];
 			bodys.setAttribute('style', 'overflow: hidden;');
 		});
